836cad8877b647220c4bf1f9a2941beaa6b9d618
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-09 18:14:45 +0900

    Update num04.cpp

--- LeetCode/learnArray/num04.cpp
+++ LeetCode/learnArray/num04.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//이중 포문 보다는 포문을 두개 따로따로 쓰는 것이 복잡도가 더 낮다. 풀이과정은 0인거 갯수를 찾아내서 머리썼다
     void duplicateZeros(vector<int>& arr) {
         for(int i=0;i<arr.size();i++){
             if((arr[i]==0)&&(arr.size()>1)){
