af5d2b03f0a01d54bf31152fd3fa1b67c8f38c74
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-10 22:04:08 +0900

    Update num02.cpp

--- Programmers/Level1/num02.cpp
+++ Programmers/Level1/num02.cpp
@@ -2,11 +2,13 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-
+//소수찾기-시간초과로 패스못함
 int solution(int n) {
     int answer = n-1;
     int flag=1;
     for(int i=n;i>1;i--){
+        if((i>8)&&i%2==0) answer--;
+        else{
         for(int j=2;j<i;j++){
 
             if(i%j==0) {
@@ -14,7 +16,7 @@ int solution(int n) {
              //   flag=0;
                 answer--;
                 break;}
-        }
+        }}
       // if(flag) {answer++;}
     }
     return answer;
