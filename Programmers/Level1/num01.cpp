c814054ca46e3aeca5c946c79edcb566867cabc8
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-10 22:03:20 +0900

    Create num01.cpp

---
+++ Programmers/Level1/num01.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+//2차원배열의 덧셈
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer(arr1.size(),vector <int>(arr1[0].size(),0));
+  cout<<arr1.size()<<" "<<arr1[0].size()<<endl;
+    for(int i=0;i<arr1.size();i++){
+        for(int j=0;j<arr1[i].size();j++){
+
+            answer[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+    return answer;
+}
