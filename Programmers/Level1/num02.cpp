c0c2cc3eb72e91387eb54da186c615142ebe6c61
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-10 22:08:58 +0900

    소수찾기

--- Programmers/Level1/num02.cpp
+++ Programmers/Level1/num02.cpp
@@ -7,13 +7,14 @@ int solution(int n) {
     int answer = n-1;
     int flag=1;
     for(int i=n;i>1;i--){
-        if((i>8)&&i%2==0) answer--;
+        //케이스 추가하면 해결될 듯!
+        if((i>7)&&((i%2==0)||(i%3==0)||(i%5==0)||i%7==0) answer--;
         else{
         for(int j=2;j<i;j++){
 
             if(i%j==0) {
               //  cout<<i<<" "<<j<<endl;
-             //   flag=0;
+              //  flag=0;
                 answer--;
                 break;}
         }}
