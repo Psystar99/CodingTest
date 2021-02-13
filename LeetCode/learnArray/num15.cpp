87c3c3ff1a03b0e68ecca69dd6125b1610ed424b
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-13 22:16:09 +0900

    끝에서 가장 3번째로 큰 수 찾기

--- LeetCode/learnArray/num15.cpp
+++ LeetCode/learnArray/num15.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        //아 같은 숫자가 있는 케이스를 생각하지 못함.ex. [2,2,3,1]....erase 기억하자! 
+        //아 같은 숫자가 있는 케이스를 생각하지 못함.ex. [2,2,3,1]....
+        //erase 기억하자! 
         int size=nums.size();
         sort(nums.begin(),nums.end());
         nums.erase(unique(nums.begin(),nums.end()),nums.end());
