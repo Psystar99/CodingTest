bccf5f938f130092946145f5bc2aa1586b6f70ea
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-13 18:18:57 +0900

    Update num03.sql

--- MySQL/Programmers/StringDate/num03.sql
+++ MySQL/Programmers/StringDate/num03.sql
@@ -1,4 +1,4 @@
--- 코드를 입력하세요
+--  IF(SEX_UPON_INTAKE REGEXP 'Neutered|Spayed', 'O' , 'X') AS 중성화 더 깔끔하게 표현 가능!
 SELECT animal_id, name, 
 if((sex_upon_intake like '%Neutered%')
    or (sex_upon_intake like '%Spayed%'),'O','X') as 중성화
