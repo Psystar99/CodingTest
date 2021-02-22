914ed91b7ba20f9567bf8b9bf2ad8db8ab66fa5d
Psystar99 <40908279+Psystar99@users.noreply.github.com>
2021-02-22 16:28:41 +0900

    Update 176.sql

--- MySQL/LeetCode/Easy/176.sql
+++ MySQL/LeetCode/Easy/176.sql
@@ -2,3 +2,13 @@
 select MAX(Salary) as SecondhighestSalary
 from Employee
 where Salary < (SELECT MAX(Salary) from Employee)
+
+#처음에 하려던 방법의 정답 if null안에 통째로 넣어야 
+SELECT 
+    IFNULL(
+        (SELECT Distinct Salary
+        FROM Employee
+        ORDER BY Salary Desc
+        LIMIT 1 OFFSET 1),
+        NULL
+    ) as SecondHighestSalary
